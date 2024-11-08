425d4cf58fef02c741f6718086c5d2e07d468e91
MarianaLGM <mariana_lobeto@hotmail.com>
2024-11-08 19:45:46 +0100

    add index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
-//let opciones = ["piedra", "papel", "tijera"];
-var opciones = [0, 1, 2];
-var eleccionMaquina;
+
+let contadorUsuario= 0;
+let contadorOrdenador=0;
+let resultados=""
+
+const botonPiedra = document.getElementById("piedra");
+const botonPapel = document.getElementById("papel");
+const botonTijera = document.getElementById("tijera");
+
+botonPiedra.addEventListener("click", function() {
+    inicioJuego ("piedra");
+});
+botonPapel.addEventListener("click", function() {
+    inicioJuego ("papel");
+});
+botonTijera.addEventListener("click", function() {
+    inicioJuego ("tijera");
+});
+
+
+function inicioJuego (miOpcion){
+    const opciones=["piedra", "papel", "tijera"]
+    const opcionOrdenador = opciones[Math.floor(Math.random()*3)]   
+
+if (miOpcion===opcionOrdenador){
+    resultados= "Empate"
+
+}else if (
+    (miOpcion === "piedra" && opcionOrdenador === "tijera") 
+){
+    resultados= "Ganaste"
+    contadorUsuario ++
+}else if (   
+    (miOpcion === "papel" && opcionOrdenador === "piedra") 
+){
+    resultados="Ganaste"
+    contadorUsuario ++
+}else if (   
+    (miOpcion === "tijera" && opcionOrdenador === "papel")         
+){
+    resultados="Ganaste"
+    contadorUsuario ++
+
+
+}else{
+    resultados="Perdiste"
+    contadorOrdenador ++
+}
+ document.getElementById("resultado-final").textContent= resultados
+ document.getElementById("contador-usuario").textContent= "Tus puntos: " + contadorUsuario
+ document.getElementById("contador-ordenador").textContent= "Puntos de la máquina: " + contadorOrdenador
+ 
+}
